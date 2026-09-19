e60e9704fdb049e82d27845e9dce1a29a1d9c0ca
PcButNoBTC <22jv96wym9@privaterelay.appleid.com>
2026-09-19 16:53:18 -0400

    Expose signed updater readiness and install controls to renderer

--- electron/preload.js
+++ electron/preload.js
@@ -3,5 +3,7 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("dreamcoderDesktop", {
   isDesktop: true,
   platform: process.platform,
-  chooseFolder: () => ipcRenderer.invoke("dreamcoder:choose-folder")
+  chooseFolder: () => ipcRenderer.invoke("dreamcoder:choose-folder"),
+  installUpdate: () => ipcRenderer.invoke("dreamcoder:install-update"),
+  onUpdateReady: (fn) => ipcRenderer.on("dreamcoder:update-ready", (_e, info) => fn(info))
 });
