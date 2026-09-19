674f01b5b41d30b57b8ac1de5cdd691c7b046c7d
PcButNoBTC <22jv96wym9@privaterelay.appleid.com>
2026-09-19 16:50:47 -0400

    Fix E2E dependency resolution from repository test directory

--- e2e/smoke.spec.js
+++ e2e/smoke.spec.js
@@ -1,4 +1,4 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require('../electron/node_modules/@playwright/test');
 test('DreamCoder frontend loads', async ({ page }) => {
   await page.goto('file://' + process.cwd() + '/../frontend/index.html');
   await expect(page.locator('body')).toBeVisible();
